59732984a138c6b73c55bdb816b964a98cdfe570
huangweizhe <naruto60913@gmail.com>
2024-07-08 19:39:00 +0800

    初始化

---
+++ app/src/main/java/com/huangliner/prioritytodo/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.huangliner.prioritytodo.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.huangliner.prioritytodo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
