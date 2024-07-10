6b30b565cba05c35843fc0519680b12faf31956f
huangweizhe <naruto60913@gmail.com>
2024-07-11 07:00:11 +0800

    新增畫面新增完成icon

--- app/src/main/java/com/huangliner/prioritytodo/presentation/ui/fragment/AddTaskFragment.kt
+++ app/src/main/java/com/huangliner/prioritytodo/presentation/ui/fragment/AddTaskFragment.kt
@@ -1,10 +1,8 @@
 package com.huangliner.prioritytodo.presentation.ui.fragment
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.huangliner.prioritytodo.R
 import com.huangliner.prioritytodo.databinding.FragmentAddAccountBinding
 import com.huangliner.prioritytodo.databinding.FragmentAddTaskBinding
@@ -18,7 +16,16 @@ class AddTaskFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAddTaskBinding.inflate(layoutInflater)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu,menu)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
