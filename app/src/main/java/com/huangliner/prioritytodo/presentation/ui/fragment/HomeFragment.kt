6d3dc5c8ab512753c1a1ef0c51f82340feb32013
huangweizhe <naruto60913@gmail.com>
2024-07-11 06:57:01 +0800

    主畫面新增搜尋icon

--- app/src/main/java/com/huangliner/prioritytodo/presentation/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/huangliner/prioritytodo/presentation/ui/fragment/HomeFragment.kt
@@ -1,10 +1,8 @@
 package com.huangliner.prioritytodo.presentation.ui.fragment
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.huangliner.prioritytodo.R
 import com.huangliner.prioritytodo.databinding.FragmentHomeBinding
@@ -23,7 +21,12 @@ class HomeFragment : Fragment() {
 
             findNavController().navigate(R.id.addTaskFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_fragment_menu,menu)
     }
 
     override fun onDestroy() {
