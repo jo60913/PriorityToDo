579cad1b37495a3089503abd6c48dcbe82ff80be
huangweizhe <naruto60913@gmail.com>
2024-07-13 16:01:06 +0800

    新增剩餘時間

--- app/src/main/java/com/huangliner/prioritytodo/presentation/ui/bindingadapter/RowItemBindingAdapter.kt
+++ app/src/main/java/com/huangliner/prioritytodo/presentation/ui/bindingadapter/RowItemBindingAdapter.kt
@@ -1,9 +1,12 @@
 package com.huangliner.prioritytodo.presentation.ui.bindingadapter
 
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.databinding.BindingAdapter
 import com.huangliner.prioritytodo.R
 import com.huangliner.prioritytodo.application.util.Priority
+import java.time.Duration
+import java.time.LocalDateTime
 
 class RowItemBindingAdapter {
     companion object{
@@ -15,6 +18,13 @@ class RowItemBindingAdapter {
                 Priority.Medium -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.priority_medium)) }
                 Priority.Low -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.priority_low)) }
             }
+        }
+
+        @BindingAdapter("app:remainTime")
+        @JvmStatic
+        fun parsePriorityColor(textview: TextView, dueDate: LocalDateTime){
+            val remainTime = Duration.between(LocalDateTime.now(),dueDate)
+            textview.text = String.format(textview.context.getString(R.string.remain_time_format),textview.context.getString(R.string.row_item_remain_time),remainTime.toHours(),remainTime.toMinutes()%60)
         }
     }
 }
